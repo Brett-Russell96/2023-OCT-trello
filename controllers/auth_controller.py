89cbd92ba5d7dee2b051dec55326f5170662b91c
Brett-Russell96 <brett.russell2016@gmail.com>
2024-02-17 11:35:23 +1100

    midifications

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -13,43 +13,41 @@ auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 @auth_bp.route("/register", methods=["POST"]) # /auth/register
 def auth_register():
     try:
-        # the data we get from the body of the request
+        # the data that we get in body of the request
         body_data = request.get_json()
-        password = body_data.get('password')
 
-            # create the user instance
+        # create the user instance
         user = User(
             name=body_data.get('name'),
-            email=body_data.get('email'),
+            email=body_data.get('email')
         )
-        #password from the request body
+
+        # password from the request body
         password = body_data.get('password')
         # if password exists, hash the password
         if password:
             user.password = bcrypt.generate_password_hash(password).decode('utf-8')
-        # add and commit user to db
+
+        # add and commit the user to DB
         db.session.add(user)
         db.session.commit()
-        # return back to the client
+        # Repond back to the client
         return user_schema.dump(user), 201
 
     except IntegrityError as err:
-        # print(err.orig.pgcode)
-        # print(err.orig.diag.column_name)
         if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
-            return {"error": f"{err.orig.diag.column_name} is required"}
+            return {"error": f"The {err.orig.diag.column_name} is required"}
         if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
             return {"error": "Email address already in use"}, 409
-
     
-@auth_bp.route("/login", methods=["POST"]) #/auth/login
+@auth_bp.route("/login", methods=["POST"]) # /auth/login
 def auth_login():
-    # get the request body
+    # get the data from the request body
     body_data = request.get_json()
-    # find the user with the email address
+    # Find the user with the email address
     stmt = db.select(User).filter_by(email=body_data.get("email"))
     user = db.session.scalar(stmt)
-    # if user exists and password is correct
+    # If user exists and password is correct
     if user and bcrypt.check_password_hash(user.password, body_data.get("password")):
         # create jwt
         token = create_access_token(identity=str(user.id), expires_delta=timedelta(days=1))
--- models/user.py
+++ models/user.py
@@ -12,12 +12,25 @@ class User(db.Model):
 
     cards = db.relationship('Card', back_populates='user', cascade='all, delete')
 
+    # {id: 1, name: User 1, email: user1@email.com}
+    # {
+    #   id: 1,
+    #   name: User 1,
+    #   email: user1@email.com,
+    #   cards: [
+    #       {id: 1, title: Card 1},
+    #       {id: 3, title: Card 3},
+    #       {id: 7, title: Card 7}
+    #   ]
+    # }
+
+
 class UserSchema(ma.Schema):
 
-    user = fields.List(fields.Nested('CardSchema', exclude=['user']))
+    cards = fields.List(fields.Nested('CardSchema', exclude=['user']))
 
     class Meta:
-        fields = ('id', 'name', 'email', 'password', 'is_admin')
+        fields = ('id', 'name', 'email', 'password', 'is_admin', 'cards')
 
-user_schema = UserSchema(exclude=['password'])
-users_schema = UserSchema(many=True, exclude=['password'])
+user_schema = UserSchema(exclude=['password']) # {}
+users_schema = UserSchema(many=True, exclude=['password']) # [{}, {}, {}]
